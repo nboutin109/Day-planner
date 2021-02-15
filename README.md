7ac3a45cb6e8749708750c4a356cc3aae4b09115
Nick Boutin <nboutin109@gmail.com>
2021-02-15 17:26:57 -0500

    organizing-C

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Title
-Simple day planner
+Day planner browser application
 
-# Purpose
-A day planner application which allows the user to create events on a daily basis and add a basic description of the event
+## Purpose
+A day planner application which allows the user to create events on a daily basis and add a basic description of the event.  
+Also shows current time.
+
+## Dependencies
+jQuery and moment.js were both used in this application.
--- script.js
+++ script.js
@@ -1,48 +1,49 @@
 $(function () {
-update();
-classCheck();
-storageCheck();
-setInterval(update, 1000);
-setInterval(classCheck, 1000);
+    update();
+    classCheck();
+    storageCheck();
+    setInterval(update, 1000);
+    setInterval(classCheck, 1000);
 
-$(".saveBtn").on("click", function() {
-    var value = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-    localStorage.setItem(time, value);
-  });
+    $(".saveBtn").on("click", function () {
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, value);
+    });
 
-function storageCheck(){
-    for (i = 9; i < 18; i++){
-        var grab = '#' +i;
-        $(grab + " .description").val(localStorage.getItem(i))
+    function storageCheck() {
+        for (i = 9; i < 18; i++) {
+            var grab = '#' + i;
+            $(grab + " .description").val(localStorage.getItem(i))
+        }
     }
-}
 
-function classCheck() {
-    for (i = 09; i < 18; i++) {
-        var grab = '#' + i;
-        var x = i;
-        var y = moment().format('HH')
-        if(x == y){
-            $(grab).removeClass('present past future');
-            $(grab).addClass('present');
+    function classCheck() {
+        for (i = 09; i < 18; i++) {
+            var grab = '#' + i;
+            var x = i;
+            var y = moment().format('HH')
+            if (x == y) {
+                $(grab).removeClass('present past future');
+                $(grab).addClass('present');
+            }
+            if (x > y) {
+                $(grab).removeClass('present past future');
+                $(grab).addClass('future')
+            }
+            if (x < y) {
+                $(grab).removeClass('present past future');
+                $(grab).addClass('past')
+            }
         }
-        if(x > y){
-            $(grab).removeClass('present past future');
-            $(grab).addClass('future')
-        }
-        if(x < y){
-            $(grab).removeClass('present past future');
-            $(grab).addClass('past')
-        }
-}}
+    }
 
-function update(){
-    var date = moment().format('MMMM Do YYYY');
-    var time = moment().format('h:mm:ss a');
-    $('#currentDay').html(date);
-    $('#currentTime').html(time);
-}
+    function update() {
+        var date = moment().format('MMMM Do YYYY');
+        var time = moment().format('h:mm:ss a');
+        $('#currentDay').html(date);
+        $('#currentTime').html(time);
+    }
 
 
 });
